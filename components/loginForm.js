46c19be87f05028e057158798b0e78a69a36fafa
Ingela <59916738+ingeliza@users.noreply.github.com>
2021-10-27 11:16:34 +0300

    button to loginForm

--- components/loginForm.js
+++ components/loginForm.js
@@ -1,6 +1,8 @@
 import Form from 'react-bootstrap/Form'
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
+import Button from 'react-bootstrap/Button'
+import Link from 'next/link'
 
 export default function LoginForm(){
     return(
@@ -16,12 +18,19 @@ export default function LoginForm(){
 
             <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
                 <Form.Label column sm="2">
-                Password
+                    Password
                 </Form.Label>
                 <Col sm="10">
-                <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" placeholder="Password" />
+                    <Link href="/">
+                        <a>Unustasid parooli?</a>
+                    </Link>
                 </Col>
             </Form.Group>
+
+            <Button variant="primary" type="submit">
+                Logi sisse
+            </Button>
         </Form>
     )
 }
