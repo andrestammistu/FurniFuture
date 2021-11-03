646a13d60f2a19c67112d38942037db2a6f307a4
Ingela <59916738+ingeliza@users.noreply.github.com>
2021-11-03 10:59:18 +0200

    added accordion component

--- components/products_layout.js
+++ components/products_layout.js
@@ -1,10 +1,12 @@
 import Header from "./header.js"
+import Accordion from "./accordion.js"
 
 
 export default function Layout(){
     return(
         <div>
             <Header></Header>
+            <Accordion></Accordion>
         </div>
     )
 }
