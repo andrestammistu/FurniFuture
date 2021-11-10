a6fb0ca462832e54977fee07624b78b324f10dd9
Ingela <59916738+ingeliza@users.noreply.github.com>
2021-11-10 10:44:51 +0200

    added accordion

--- components/nav.js
+++ components/nav.js
@@ -1,17 +1,60 @@
-import Nav from 'react-bootstrap/Nav'
+import Accordion from 'react-bootstrap/Accordion'
 
 
 export default function NavItems(){
     return(
         <div style={{width:"15%"}}>
-            <Nav defaultActiveKey="/home" className="flex-column">
-                <Nav.Link href="/home">Active</Nav.Link>
-                <Nav.Link eventKey="link-1">Link</Nav.Link>
-                <Nav.Link eventKey="link-2">Link</Nav.Link>
-                <Nav.Link eventKey="disabled" disabled>
-                    Disabled
-                </Nav.Link>
-            </Nav>
+            <Accordion flush>
+                <Accordion.Item eventKey="0" disabled>
+                    <Accordion.Header>Elutuba</Accordion.Header>
+                    <Accordion.Body>
+                        <input type="checkbox"/><br/>
+                        <input type="checkbox"/><br/>
+                        <input type="checkbox"/>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                    <Accordion.Header>Magamistuba</Accordion.Header>
+                    <Accordion.Body>
+                        <input type="checkbox"/><br/>
+                        <input type="checkbox"/><br/>
+                        <input type="checkbox"/>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="2">
+                    <Accordion.Header>Kontor</Accordion.Header>
+                    <Accordion.Body>
+                        <input type="checkbox"/><br/>
+                        <input type="checkbox"/><br/>
+                        <input type="checkbox"/>
+
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="3">
+                    <Accordion.Header>Köök</Accordion.Header>
+                    <Accordion.Body>
+                        <input type="checkbox"/><br/>
+                        <input type="checkbox"/><br/>
+                        <input type="checkbox"/>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="4">
+                    <Accordion.Header>Vannituba</Accordion.Header>
+                    <Accordion.Body>
+                        <input type="checkbox"/><br/>
+                        <input type="checkbox"/><br/>
+                        <input type="checkbox"/>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="5">
+                    <Accordion.Header>Sisekujundus</Accordion.Header>
+                    <Accordion.Body>
+                        <input type="checkbox"/><br/>
+                        <input type="checkbox"/><br/>
+                        <input type="checkbox"/>
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
         </div>
     )
 }
