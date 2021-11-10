d21c3d6adb04fbab1834dc03de3efdba33d31985
Ingela <59916738+ingeliza@users.noreply.github.com>
2021-11-10 11:31:25 +0200

    dropdowns into filter function

--- components/filter.js
+++ components/filter.js
@@ -1,16 +1,11 @@
+import { Nav } from "react-bootstrap";
+
 export default function Filter(){
     return(
         <Nav justify variant="tabs" defaultActiveKey="/home">
-            {/* <Nav.Item>
-                <Nav.Link href="/home">Active</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="link-1">Loooonger NavLink</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="link-2">Link</Nav.Link>
-            </Nav.Item> */}
-            <NavDropdown title="Filtreeri" id="nav-dropdown">
+            <NavDropdown title="Hinnavahemik" id="nav-dropdown">
+            </NavDropdown>
+            <NavDropdown title="Sorteeri" id="nav-dropdown">
                 <NavDropdown.Item eventKey="4.1">Kõrgem hind esiteks</NavDropdown.Item>
                 <NavDropdown.Item eventKey="4.2">Madalam hind esiteks</NavDropdown.Item>
                 <NavDropdown.Item eventKey="4.3">Tähestikulises järjekorras</NavDropdown.Item>
