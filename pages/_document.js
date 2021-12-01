4104bcc7ff7b8b7324cf2812cbd93e6ebe7f407e
Ingela <59916738+ingeliza@users.noreply.github.com>
2021-12-01 11:21:13 +0200

    Script from _document.js to _app.js file eslint error

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,5 @@
 // pages/_document.js
 import Document, { Html, Main, Head, NextScript } from 'next/document'
-import Script from 'next/script'
 
 class MyDocument extends Document {
   render() {
@@ -13,11 +12,6 @@ class MyDocument extends Document {
                     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" 
                     crossOrigin="anonymous">
                 </link>
-                <Script 
-                    src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
-                    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" 
-                    crossOrigin="anonymous">
-                </Script>
                 <link
                     href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200&display=swap"
                     rel="stylesheet"
