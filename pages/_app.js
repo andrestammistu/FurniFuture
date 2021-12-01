e51638128b2429fcaf2a19fa936c02ce65686d80
Ingela <59916738+ingeliza@users.noreply.github.com>
2021-12-01 11:13:38 +0200

    title and Script should not be used in _document.js eslint error

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,22 @@
+import Head from 'next/head'
+import Script from 'next/script'
 import '../styles/globals.css'
 
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+
+      <Head>
+        <Script 
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
+            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" 
+            crossOrigin="anonymous">
+        </Script>
+      </Head>
+      
+      <Component {...pageProps} />
+
+    </div>
+  )
 }
